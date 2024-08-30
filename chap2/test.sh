598347ee1a0c28b89e2e2b78b77c05b2c8af7cbc
Yann Debray <debray.yann@gmail.com>
2024-08-30 11:00:55 +0200

    Add chap2 API call test

---
+++ chap2/test.sh
@@ -0,0 +1,9 @@
+# export OPENAI_API_KEY="sk-1234567890abcdef1234567890abcdef"
+
+curl https://api.openai.com/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $OPENAI_API_KEY" \
+  -d '{
+     "model": "gpt-4o-mini",
+     "messages": [{"role": "user", "content": "Say this is a test!"}]
+   }'
